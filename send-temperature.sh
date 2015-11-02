d6257ce9ac70e077f3c6af1bf2aca8220bc7289b
David Gwartney <david_gwartney@gmail.com>
2015-11-02 16:24:10 +0000

    Working script

--- send-temperature.sh
+++ send-temperature.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 
-metric=
-source=
-
 while :
 do
   temperature="$(vcgencmd measure_temp | cut -d '=' -f 2 | tr -d "'C")"
-  echo "$temperature"
+  measurement-create -e $BOUNDARY_EMAIL -t $BOUNDARY_API_TOKEN -n "PI_TEMPERATURE" -m "$temperature"
   sleep 1
 done
